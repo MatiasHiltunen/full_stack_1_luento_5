4f32f3f37dd5203ff05735b241976ccf33612057
Matias Hiltunen <26164588+MatiasHiltunen@users.noreply.github.com>
2024-09-27 09:02:40 +0300

    add drizzle

--- README.md
+++ README.md
@@ -1,7 +1,41 @@
+## FullStack 1 opintojakson luentoon 5 liittyvä esimerkki
+
+### Backend
+
+_Backend-kirjastot ovat projektissa valittu niin että ne tukevat myös JS/TS pohjaisia edge-funktioiden suoritusympäristöjä_
+
+Projektissa on otettu käyttöön TypeScript sovelluskehys Hono
+- Linkki Honon dokumentaatioon: https://hono.dev/
+
+Tietokantana (toistaiseksi)
+- sqlite https://www.npmjs.com/package/better-sqlite3
+- ORM:ina drizzle https://orm.drizzle.team/
+
+## #Käyttöönotto
+1. Kloonaa repositorio
+
+2. Asenna paketit
+
 ```
 npm install
+```
+
+3. Suorita tietokannan luominen schema.ts tiedoston mukaisesti
+```
+npm run generate
+```
+
+4. Suorita migraatio
+```
+npm run migrate
+```
+
+5. Käynnistä backend-palvelin kehitysympäristössä
+```
 npm run dev
 ```
+
+
 
 ```
 open http://localhost:3000
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,60 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import bcrypt from 'bcryptjs'
-import { randomUUID, UUID } from 'crypto'
-import './db'
 
-type User = {
-  id: UUID
-  username: string
-  password: string
-  email: string
+import { db } from './db'
+import { users } from './schema'
 
-}
+// Luodaan app -muuttujaan Hono instanssi johon esim. user instanssi kytketään
+// /user polun luomiseksi
+const app = new Hono()
 
-type FakeDb = {
-  [key:UUID]: User
-}
-
-const fake_db: FakeDb = {}
-
+// Luodaan user -muuttujaan Hono instanssi jota käytetään /user polussa
 const user = new Hono()
 
-user.get('/', (c) => {
+// Vastaa polkuun /user tehtyihin HTTP GET kyselyihin
+user.get('/', async (c) => {
 
-  return c.json(fake_db)
+  // Hakee kaikki käyttäjät tietokannasta
+  const allUsers = await db.select({
+    id: users.id,
+    username: users.username,
+    email: users.email
+  }).from(users)
+  
+  return c.json(allUsers)
 
 })
 
-
+// Vastaa polkuun /user tehtyihin HTTP POST kyselyihin
 user.post("/", async (c)=>{
 
+  // Parsii POST-requestin bodystä JSON muotoisen datan JS/TS objektiksi
   const user = await c.req.json()
 
+  // Tarkistetaan onko clientin lähettämässä datassa password avaimella dataa
   if(user.password == null) {
     return c.text("Salasana ei voi olla tyhjä", 400)
   }
 
+  // Demonstroitu salasanan muuttaminen selväkielisestä tiivisteeksi (hash)
+  // käyttämällä bcrypt algoritmia
   const hashedPassword = await bcrypt.hash(user.password, 8)
 
-  const uuid = randomUUID()
-
-  fake_db[uuid] = {
-    id: uuid,
+  // Lisätään uusi käyttäjä tietokantaan
+  const createdUser = await db.insert(users).values({
     username: user.username,
+    email: user.email,
     password: hashedPassword,
-    email: user.email
-  }
-
-  return c.json({
-    id: uuid,
-    username: user.username,
-    email: user.email
-  })
+  }).returning({id: users.id})
+  
+  // Lehttää clientille luodun käyttäjän id:n vastauksena
+  // HTTP statuskoodi 201 - Created, lisätty mukaan
+  return c.json(createdUser, 201)
 })
 
 
-const app = new Hono()
-
+// Kytketään user-muuttujassa oleva Hono-instanssi /user polkuun
 app.route("/user", user)
 
 app.get('/', (c) => {
